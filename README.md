a8fc45b7ce1227ed6d30ad48b65c5e198177aa6a
TLWebdesign <info@tlwebdesign.nl>
2024-02-21 01:56:06 +0100

    First Commit
    First Version of the unified EOL Security Fixes Plugin

---
+++ README.md
@@ -0,0 +1,15 @@
+# Joomla 3 EOL Security Fixes
+ 
+This plugin will help you update the files associated with the known security fixes as listed below.
+- [20231101] - Core - Exposure of environment variables. More info on the vulnerability here: https://developer.joomla.org/security-centre/919-20231101-core-exposure-of-environment-variables.html
+- [20240201] - Core - Insufficient session expiration in MFA management views. More info on the vulnerability here: https://developer.joomla.org/security-centre/925-20240201-core-insufficient-session-expiration-in-mfa-management-views.html
+- [20240202] - Core - Open redirect in installation application. More info on the vulnerability here: https://developer.joomla.org/security-centre/926-20240202-core-open-redirect-in-installation-application.html
+- [20240203] - Core - XSS in media selection fields. More info on the vulnerability here: https://developer.joomla.org/security-centre/927-20240203-core-xss-in-media-selection-fields.html
+- [20240205] - Core - Inadequate content filtering within the filter code. More info on the vulnerability here: https://developer.joomla.org/security-centre/929-20240205-core-inadequate-content-filtering-within-the-filter-code.html
+
+## Donate to the joomla project!
+If this plugin saved you valuable time please consider donating something to the joomla project: https://community.joomla.org/donate. 
+Especially agencies who will save tons of time when they have multiple websites still on J3. Any donation is much appreciated.
+
+## Backup First!
+Always try this fix first on a test environment because it could potentially break language files that were not following exact specification. Previously these language files would still work but in fixing the vulnerability the strictness of how these files are processed makes it that a language string can not have new lines in the content anymore.
---
+++ script.php
@@ -0,0 +1,74 @@
+<?php
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Installer\Installer;
+
+class  joomla3eolsecurityfixesInstallerScript
+{
+    function preflight($type, $parent)
+    {
+
+
+
+        // Get the "files" directory path from the plugin's installation package
+        $sourcePath = $parent->getParent()->getPath('source') . '/files';
+
+        // Define the root path of your Joomla installation
+        $rootPath = JPATH_ROOT;
+
+        // Retrieve all the files from the "files" directory and its subdirectories
+        $files = Folder::files($sourcePath, '.', true, true);
+//        echo "<pre>";
+//        var_dump($files);
+//        die();
+
+        foreach ($files as $file) {
+            // Calculate the relative path from the "files" directory
+            $relativePath = str_replace($sourcePath, '', $file);
+
+            // Determine the destination path in the root directory
+            $destPath = $rootPath . '/' . $relativePath;
+
+            // Check if the file exists in the destination path
+            if (File::exists($destPath)) {
+                if (!File::copy($file, $destPath, '', true)) {
+                    Factory::getApplication()->enqueueMessage("Failed to replace $relativePath", 'error');
+                } else {
+                    Factory::getApplication()->enqueueMessage("Replaced $relativePath", 'message');
+                }
+            } else {
+                JError::raiseWarning(500, "original File not found: " . $relativePath);
+            }
+        }
+
+        // Remove this plugin to leave no trace
+        $this->uninstallPlugin();
+
+        return true;
+    }
+
+    private function uninstallPlugin()
+    {
+        $plugins = $this->findThisPlugin();
+        foreach ($plugins as $plugin) {
+            Installer::getInstance()->uninstall($plugin->type, $plugin->extension_id);
+        }
+    }
+
+    private function findThisPlugin()
+    {
+        $db    = Factory::getDbo();
+        $query = $db->getQuery(true)
+            ->select(array('extension_id', 'type'))
+            ->from('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote('joomla3eolsecurityfixes'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('file'));
+        $db->setQuery($query);
+
+        return $db->loadObjectList();
+    }
+}
