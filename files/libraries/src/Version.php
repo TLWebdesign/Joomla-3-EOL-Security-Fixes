12e2a397e45e17dee02e6dcc7d8a81d37ea5c6b6
TLWebdesign <info@tlwebdesign.nl>
2024-03-04 16:28:43 +0100

    Fixed EXTRA_VERSION so it does not trigger update notice
    Switched the version name around so it starts with a number. Seems a string will trigger an update notice. Fixes #5

--- files/libraries/src/Version.php
+++ files/libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    const EXTRA_VERSION = 'EOLfix-2024-02-21';
+    const EXTRA_VERSION = '2024-02-21-EOLfix';
 
     /**
      * Release version.
